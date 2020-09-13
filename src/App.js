6ce2fb545de0e722b804c34d1531ca0b74f1724d
chamodya wimansha <chamodyawimansha@gmail.com>
2020-09-13 19:29:04 +0530

    straightforward design

--- src/App.js
+++ src/App.js
@@ -26,41 +26,8 @@ class App extends Component {
   render() {
     return (
       <div id="drum-machine">
-        <div id="drum-machine-panel">
-          <div id="drum-machine-power">
-            <p id="drum-machine-power-label" className="select-off">
-              Power
-            </p>
-            <label class="switch">
-              <input type="checkbox" />
-              <span class="slider"></span>
-            </label>
-          </div>
-
-          <div id="drum-machine-display"></div>
-
-          <div id="drum-machine-mode">
-            <p id="drum-machine-mode-label" className="select-off">
-              Mode
-            </p>
-            <span className="drum-machine-mode-label select-off">A</span>
-            <label class="switch">
-              <input type="checkbox" />
-              <span class="slider"></span>
-            </label>
-            <span className="drum-machine-mode-label select-off">B</span>
-          </div>
-          <div id="volume-slider">
-            <input
-              type="range"
-              min="1"
-              max="100"
-              value="50"
-              class="volume-slider"
-              id="volume-slider"
-            />
-          </div>
-        </div>
+        <div id="switch-panel"></div>
+        <div id="button-panel"></div>
       </div>
     );
   }
