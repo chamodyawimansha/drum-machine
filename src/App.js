42e88b0398c36d1ab5f37d3d31257409b426a8ce
chamodya wimansha <chamodyawimansha@gmail.com>
2020-09-19 10:17:35 +0530

    a function created to change sound modes

--- src/App.js
+++ src/App.js
@@ -10,143 +10,147 @@ class App extends Component {
       power: false,
       volume: 0,
       text: "",
-      modeOne: [
-        {
-          keyCode: 81,
-          keyTrigger: "Q",
-          name: "Heater 1",
-          id: "Heater-1",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-        },
-        {
-          keyCode: 87,
-          keyTrigger: "W",
-          name: "Heater 2",
-          id: "Heater-2",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-        },
-        {
-          keyCode: 69,
-          keyTrigger: "E",
-          name: "Heater 3",
-          id: "Heater-3",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-        },
-        {
-          keyCode: 65,
-          keyTrigger: "A",
-          name: "Heater 4",
-          id: "Heater-4",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-        },
-        {
-          keyCode: 83,
-          keyTrigger: "S",
-          name: "Clap",
-          id: "Clap",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-        },
-        {
-          keyCode: 68,
-          keyTrigger: "D",
-          name: "Open HH",
-          id: "Open-HH",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-        },
-        {
-          keyCode: 90,
-          keyTrigger: "Z",
-          name: "Kick n' Hat",
-          id: "Kick-n'-Hat",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-        },
-        {
-          keyCode: 88,
-          keyTrigger: "X",
-          name: "Kick",
-          id: "Kick",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-        },
-        {
-          keyCode: 67,
-          keyTrigger: "C",
-          name: "Closed HH",
-          id: "Closed-HH",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-        },
-      ],
-      modeTwo: [
-        {
-          keyCode: 81,
-          keyTrigger: "Q",
-          name: "Chord 1",
-          id: "Chord_1",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
-        },
-        {
-          keyCode: 87,
-          keyTrigger: "W",
-          name: "Chord 2",
-          id: "Chord-2",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
-        },
-        {
-          keyCode: 69,
-          keyTrigger: "E",
-          name: "Chord 3",
-          id: "Chord-3",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
-        },
-        {
-          keyCode: 65,
-          keyTrigger: "A",
-          name: "Shaker",
-          id: "Heater-4",
-          url:
-            "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
-        },
-        {
-          keyCode: 83,
-          keyTrigger: "S",
-          name: "Open HH",
-          id: "Open-HH",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
-        },
-        {
-          keyCode: 68,
-          keyTrigger: "D",
-          name: "Closed HH",
-          id: "Closed-HH",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
-        },
-        {
-          keyCode: 90,
-          keyTrigger: "Z",
-          name: "Punchy Kick",
-          id: "Punchy-Kick",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
-        },
-        {
-          keyCode: 88,
-          keyTrigger: "X",
-          name: "Side-Stick",
-          id: "Side Stick",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
-        },
-        {
-          keyCode: 67,
-          keyTrigger: "C",
-          name: "Snare",
-          id: "Snare",
-          url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
-        },
-      ],
+      mode: [],
     };
+
+    this.handleDeviceOnState = this.handleDeviceOnState.bind(this);
+    this.handleModeChange = this.handleModeChange.bind(this);
     this.handleVolChange = this.handleVolChange.bind(this);
     this.handleDrumPadClick = this.handleDrumPadClick.bind(this);
-    this.handleDeviceOnState = this.handleDeviceOnState.bind(this);
-    // this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
+
+  profileOne = [
+    {
+      keyCode: 81,
+      keyTrigger: "Q",
+      name: "Heater 1",
+      id: "Heater-1",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      keyCode: 87,
+      keyTrigger: "W",
+      name: "Heater 2",
+      id: "Heater-2",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      keyCode: 69,
+      keyTrigger: "E",
+      name: "Heater 3",
+      id: "Heater-3",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      keyCode: 65,
+      keyTrigger: "A",
+      name: "Heater 4",
+      id: "Heater-4",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      keyCode: 83,
+      keyTrigger: "S",
+      name: "Clap",
+      id: "Clap",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      keyCode: 68,
+      keyTrigger: "D",
+      name: "Open HH",
+      id: "Open-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      keyCode: 90,
+      keyTrigger: "Z",
+      name: "Kick n' Hat",
+      id: "Kick-n'-Hat",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      keyCode: 88,
+      keyTrigger: "X",
+      name: "Kick",
+      id: "Kick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      keyCode: 67,
+      keyTrigger: "C",
+      name: "Closed HH",
+      id: "Closed-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+
+  profileTwo = [
+    {
+      keyCode: 81,
+      keyTrigger: "Q",
+      name: "Chord 1",
+      id: "Chord_1",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+    },
+    {
+      keyCode: 87,
+      keyTrigger: "W",
+      name: "Chord 2",
+      id: "Chord-2",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+    },
+    {
+      keyCode: 69,
+      keyTrigger: "E",
+      name: "Chord 3",
+      id: "Chord-3",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+    },
+    {
+      keyCode: 65,
+      keyTrigger: "A",
+      name: "Shaker",
+      id: "Heater-4",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+    },
+    {
+      keyCode: 83,
+      keyTrigger: "S",
+      name: "Open HH",
+      id: "Open-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+    },
+    {
+      keyCode: 68,
+      keyTrigger: "D",
+      name: "Closed HH",
+      id: "Closed-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+    },
+    {
+      keyCode: 90,
+      keyTrigger: "Z",
+      name: "Punchy Kick",
+      id: "Punchy-Kick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+    },
+    {
+      keyCode: 88,
+      keyTrigger: "X",
+      name: "Side-Stick",
+      id: "Side Stick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+    },
+    {
+      keyCode: 67,
+      keyTrigger: "C",
+      name: "Snare",
+      id: "Snare",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+    },
+  ];
 
   handleDeviceOnState() {
     this.setState((state) => ({
@@ -154,6 +158,13 @@ class App extends Component {
     }));
   }
 
+  handleModeChange() {
+    if (this.state.power === false) return;
+
+    this.setState((state) => ({
+      mode: state.mode === this.profileOne ? this.profileTwo : this.profileOne,
+    }));
+  }
   handleVolChange(event) {
     if (this.state.power === false) return;
     this.setState({
@@ -170,31 +181,37 @@ class App extends Component {
     audioElement.play();
 
     // show name on the display
-    let sound = this.state.modeOne.find((item) => item.id === event.target.id);
+    let sound = this.state.mode.find((item) => item.id === event.target.id);
     this.setState({
       text: sound.name,
     });
   }
 
-  // handleKeyPress(event) {
-  //   if (this.state.power === false) return;
-  //   let sound = this.state.modeOne.find(
-  //     (item) => item.keyCode === event.keyCode
-  //   );
+  handleKeyPress(event) {
+    if (this.state.power === false) return;
 
-  //   if (sound) {
-  //     this.playSound(sound.id);
-  //   }
-  // }
+    console.log(event.keyCode);
+
+    // let sound = this.state.modeOne.find(
+    //   (item) => item.keyCode === event.keyCode
+    // );
+
+    // if (sound) {
+    //   this.playSound(sound.id);
+    // }
+  }
   // playSound()
 
-  // componentDidMount() {
-  //   document.addEventListener("keydown", this.handleKeyPress);
-  // }
+  componentDidMount() {
+    this.setState({
+      mode: this.profileOne,
+    });
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
 
-  // componentWillUnmount() {
-  //   document.removeEventListener("keydown", this.handleKeyPress);
-  // }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
 
   //This method clears the display
   // componentDidUpdate() {
@@ -240,6 +257,7 @@ class App extends Component {
                 type="checkbox"
                 id="mode-input"
                 className="panel-switch-input"
+                onChange={this.handleModeChange}
               />
               <span id="mode-slider" className="panel-switch-slider"></span>
             </label>
@@ -260,7 +278,7 @@ class App extends Component {
           </div>
         </div>
         <div id="button-panel">
-          {this.state.modeOne.map((btn) => {
+          {this.state.mode.map((btn) => {
             return (
               <Button
                 keyTrigger={btn.keyTrigger}
